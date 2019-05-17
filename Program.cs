a470e29025c6da64dd2eb2acbf99b2f0294f2df2
Mark Pitman <mark.pitman@gettyimages.com>
2019-05-17 08:19:34 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using GettyImages.Api.Entity;
+using Newtonsoft.Json;
+
+namespace GettyImages.Api.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait(TimeSpan.FromSeconds(30));
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var apiKey = args[0];
+            var apiSecret = args[1];
+
+            Debug.Assert(!string.IsNullOrEmpty(apiKey), "API Key must have a value");
+            Debug.Assert(!string.IsNullOrEmpty(apiSecret), "API Secret must have a value");
+            var client = ApiClient.GetApiClientWithClientCredentials(apiKey, apiSecret);
+            var results = await client
+                .SearchImagesCreative()
+                .WithGraphicalStyle(GraphicalStyles.Photography)
+                .WithPhrase("food")
+                .WithSortOrder(SortOrder.MostPopular)
+                .ExecuteAsync();
+            Console.WriteLine(JsonConvert.SerializeObject(results, Formatting.Indented));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Getty Images API .NET SDK Sample
+
+This project is a very simple sample project that demonstrates basic
+usage of the Getty Images API .NET SDK. It is a console application
+that takes two command line arguments, API Key and Secret.
+
+Once the repository is cloned, use the following commands
+to try it out:
+
+```bash
+dotnet restore
+dotnet build
+dotnet run API_KEY API_SECRET
+```
